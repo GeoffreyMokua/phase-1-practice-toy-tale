5cb71f120eeda42e152ddfe3b567e3d7e1c8b01a
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-08-27 10:56:06 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -13,3 +13,55 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+function getToys() {
+  return fetch('http://localhost:3000/toys')
+    .then(res => res.json())
+}
+getToys().then(toys => {
+  toys.forEach(toy => {
+    
+    renderToys(toy)
+  })
+})
+
+function postToy(toy_data) {
+  fetch('http://localhost:3000/toys', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        "name": "Jessie",
+        "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
+        "likes": 0
+
+      })
+    })
+    .then(res => res.json())
+    .then((obj_toy) => {
+      let new_toy = renderToys(obj_toy)
+      divCollect.append(new_toy)
+    })
+}
+
+function likes(e) {
+  e.preventDefault()
+  let more = parseInt(e.target.previousElementSibling.innerText) + 1
+
+  fetch(`http://localhost:3000/toys/${e.target.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+
+      },
+      body: JSON.stringify({
+        "likes": newNumberOfLikes
+      })
+    })
+    .then(res => res.json())
+    .then((like_obj => {
+      e.target.previousElementSibling.innerText = `${newNumberOfLikes} likes`;
+    }))
+}
